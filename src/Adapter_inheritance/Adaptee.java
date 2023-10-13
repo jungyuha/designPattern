b226c5346920a5e32064e585439b0a90ebd6a978
jungyuha <yooha9621@gmail.com>
2023-10-13 14:20:04 +0900

    구조 디자인 패턴 : 어댑터 패턴(상속)

--- src/Adapter_inheritance/Adaptee.java
+++ src/Adapter_inheritance/Adaptee.java
@@ -2,6 +2,6 @@ package Adapter_inheritance;
 
 public class Adaptee {
     void oldOperation(){
-        System.out.println("old operation _ inheritance ");
+        System.out.println("@@old operation _ inheritance ");
     }
 }
